4dd018a25bdbafc13c0ee019ffe43f7c078c2895
EseToni <Falsigfx@gmail.com>
2025-04-30 21:07:09 +0200

    Refactor UpdatePortfolio component for improved readability and structure; streamline imports and enhance state management.

--- src/pages/portfolio/UpdatePortfolio.tsx
+++ src/pages/portfolio/UpdatePortfolio.tsx
@@ -1,75 +1,85 @@
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
-import {updatePortfolio } from "@lib/services/portfolio.service"
-
-import Step3TemplateSelection from "@components/common/wizard/Step3TemplateSelection"
-import Step4Preview from "@components/common/wizard/Step4Preview"
-import WizardContainer from "@components/common/wizard/WizardContainer"
-import { isAuthenticated } from "@lib/services/auth.service"
-import { useProfileStore } from "@store/profileStore"
 import { useNavigate } from "react-router-dom"
 
+import { usePortfolioStore } from "@store/portfolioStore"
+import { useProfileStore } from "@store/profileStore"
+
+import { isAuthenticated } from "@lib/services/auth.service"
+import { updatePortfolio } from "@lib/services/portfolio.service"
+
+import WizardContainer from "@components/common/wizard/WizardContainer"
+import Step3TemplateSelection from "@components/common/wizard/Step3TemplateSelection"
+import Step4Preview from "@components/common/wizard/Step4Preview"
 
 export default function UpdatePortfolio() {
-  const totalSteps = 2
-  const navigate = useNavigate();
-  const [currentStep, setCurrentStep] = useState(1)
-  const [templateName, setTemplateName] = useState<string>("")
-  const type = "portfolio"
-    const { profileStore } = useProfileStore()
-  const handle2ndStepNext = (TemplateName: string) => {
-    setTemplateName(TemplateName)
-    
-    setCurrentStep(2)
-  }
+	const totalSteps = 2
+	const navigate = useNavigate()
+	const [currentStep, setCurrentStep] = useState(1)
+	const [templateName, setTemplateName] = useState<string>("")
+	const type = "portfolio"
+	const { profileStore } = useProfileStore()
+	const { portfolioStore, updatePortfolioStore } = usePortfolioStore()
+	const handle2ndStepNext = (TemplateName: string) => {
+		setTemplateName(TemplateName)
 
-  const handle2ndStepBack = () => {
-    setCurrentStep(1)
-    setTemplateName("")
-    localStorage.removeItem("TemplateName-home")
-  }
+		setCurrentStep(2)
+	}
 
-  const handleUpdate = async () => {
-    if (!profileStore || !type || !templateName) {
-      toast.error("Por favor completa todos los pasos")
-      return
-    }
-    console.log("TemplateName", templateName)
-    await updatePortfolio({templateName})
-    navigate("/app/dashboard");
-  }
+	const handle2ndStepBack = () => {
+		setCurrentStep(1)
+		setTemplateName("")
+		localStorage.removeItem("TemplateName-home")
+	}
 
-  useEffect(() => {
-    const storedTemplateName = localStorage.getItem("TemplateName-home")
-    if (storedTemplateName) {
-      const TemplateName = JSON.parse(storedTemplateName)
-      setTemplateName(TemplateName)
-      setCurrentStep(2)
-    }
-  }, [])
+	const handleUpdate = async () => {
+		if (!profileStore || !type || !templateName) {
+			toast.error("Por favor completa todos los pasos")
+			return
+		}
+		await updatePortfolio({ templateName })
+		updatePortfolioStore({
+			...(portfolioStore?.template && {
+				template: {
+					...portfolioStore.template,
+					name: templateName,
+				},
+			}),
+		})
+		navigate("/app/dashboard")
+	}
 
-  return (
-    <main>
-      <WizardContainer currentStep={currentStep} totalSteps={totalSteps}>
-        {currentStep === 1 && (
-          <Step3TemplateSelection
-            type={type}
-            handleNext={handle2ndStepNext}
-            handleBack={() => {}}
-          />
-        )}
-        {currentStep === 2 && (
-          <Step4Preview
-            type={type}
-            profile={profileStore ?? undefined}
-            handleBack={handle2ndStepBack}
-            TemplateName={templateName}
-            handleAction={handleUpdate}
-            isAuthenticated={isAuthenticated()}
-            mode="update"
-          />
-        )}
-      </WizardContainer>
-    </main>
-  )
+	useEffect(() => {
+		const storedTemplateName = localStorage.getItem("TemplateName-home")
+		if (storedTemplateName) {
+			const TemplateName = JSON.parse(storedTemplateName)
+			setTemplateName(TemplateName)
+			setCurrentStep(2)
+		}
+	}, [])
+
+	return (
+		<main>
+			<WizardContainer currentStep={currentStep} totalSteps={totalSteps}>
+				{currentStep === 1 && (
+					<Step3TemplateSelection
+						type={type}
+						handleNext={handle2ndStepNext}
+						handleBack={() => {}}
+					/>
+				)}
+				{currentStep === 2 && (
+					<Step4Preview
+						type={type}
+						profile={profileStore ?? undefined}
+						handleBack={handle2ndStepBack}
+						TemplateName={templateName}
+						handleAction={handleUpdate}
+						isAuthenticated={isAuthenticated()}
+						mode="update"
+					/>
+				)}
+			</WizardContainer>
+		</main>
+	)
 }
